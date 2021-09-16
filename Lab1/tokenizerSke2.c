464084d17ba51b23b2db18fe6e36bc35351f58c4
Dom <dvjohnson@miners.utep.edu>
2021-09-16 11:45:02 -0600

    Added functions to tokenizerSke2.c

--- Lab1/tokenizerSke2.c
+++ Lab1/tokenizerSke2.c
@@ -6,30 +6,55 @@
    ('\t' or ' ').
    Zero terminators are not printable (therefore false) */
 bool delim_character(char c){
-
+	if(c==' '||c=='\t')
+		return 1;
+	return 0;
 }
 
 /* Return true (non-zero) if c is a non-whitespace
    character (not tab or space).
    Zero terminators are not printable (therefore false) */
 bool non_delim_character(char c){
-
+	if(c=='\0'|| c==' ' || c=='\t')
+		return 0;
+	return 1;
 }
 
 /* Returns a pointer to the first character of the next
    space-separated word*/
 char *word_start(char* str){
-
+	char *start;
+	while(*str!=' '){
+		str++;
+	}
+	str++;
+	start = str;
+	return start;
 }
 
 /* Returns a pointer to the first space character of the zero
 terminated string*/
 char *end_word(char* str){
+	char *end;
+	while(*str!=' '){
+		str++;
+	}
+	str++;
+	end = str;
+	return end;
 
 }
 // counts the number of words or tokens
 int count_tokens(char* str){
-
+	//Tokens = whitespace(n) + 1.
+	//i.e. "This is my string." 3 spaces, 4 words
+	int count = 0;
+	while(*str!='\n' && *str!='\0'){
+		if(*str==' ')
+			count++;
+		str++;
+	}
+	return count+1;
 
 }
 /* Returns a freshly allocated zero-terminated vector of freshly allocated
@@ -41,21 +66,60 @@ int count_tokens(char* str){
      tokens[3] = 0
 */
 char *copy_str(char *inStr, short len){
-
+	char *token;
+	int i=0;
+	token = (char *) malloc(len*sizeof(char));
+	while(i<len){
+		token[i] = *inStr;
+		i++;
+		inStr++;
+	}
+	
+	return token;
+		
 }
 
 char** tokenize(char* str){
-	char *tokens[][];
-	int c, i, tokens;
-	//Tokens = whitespace(n) + 1.
-	//i.e. "This is my string." 3 spaces, 4 words
-	while(c!='\n' || c!=EOF){
-		for(; *ptr!=' '; ptr++){
-			
+	char *temp = str;
+	char *copy, *start;
+	int c, i, j, tokens;
+	tokens = count_tokens(temp);
+	char** token_store[tokens][0];
+	
+	printf("Tokens: %d\n", tokens);
+	c=*temp;
+	while(c!='\n' && c!='\0'){
+		for(i=0; *temp!=' ' && (*temp!='\n' && *temp!='\0'); i++, temp++){
+			printf("Looping because Temp = %c\n", *temp);
+		}
+		printf("i is: %d\n", i);
+		copy = copy_str(start, i);
+		printf("Copied token = %c\n", *copy);
+		token_store[j][0] = copy;
+		temp++;
+		c=*temp;
+		start = temp;
+		j++;
+		
+		
+	}
+	print_all_tokens(token_store);
+	/*while(c!='\n'){
+		for(; *temp!=' '; temp++){
+			c = *temp;
+			printf("C is: %c\n", c);
+			if(delim_character(*temp)==1){
+				printf("Delim found!");
+			}
+		}
+		temp++;
+		c = *temp;		
+	}*/
 }
 
 
 void print_all_tokens(char** tokens){
+
 }
 int main(){
 	char mystring[MAXLINE];
@@ -65,6 +129,7 @@ int main(){
 	fgets(mystring, MAXLINE, stdin);
 	
 	strptr = &mystring;
+	printf("String: %s\n", mystring);
 	tokenize(strptr);
 	
 	return 0;
